05247ea616bc008081e2e1c5feb23100bfdd39dc
Dushusir <1414556676@qq.com>
2024-03-18 15:18:32 +0800

    test(formula): add test with random formulas

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10 * 1000, // 10 seconds
+  timeout: 1000 * 1000, // 10 seconds
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
