869096f09b88ca004166b71c43d9e210a1cc63b0
Dushusir <1414556676@qq.com>
2024-03-22 21:12:52 +0800

    feat: add sum,count generate script (#3)

---
+++ scripts/generate-formula-with-nested-sum.js
@@ -0,0 +1,43 @@
+import fs from 'fs'
+
+const rows = 5001;
+const cols = 20;
+let maxCol = chatAtABC(10);
+let matrix = {};
+
+for (let row = 0; row < rows; row++) {
+    matrix[row] = {};
+    for (let col = 0; col < cols; col++) {
+        if (row === 0) {
+            // Fill the first row with random numbers
+            matrix[row][col] = {v: 1};
+        } else {
+
+            matrix[row][col] = {f: `=SUM(${chatAtABC(col)}${row}+1)`};
+        }
+    }
+}
+
+function chatAtABC(n) {
+    const ord_a = 'a'.charCodeAt(0);
+
+    const ord_z = 'z'.charCodeAt(0);
+
+    const len = ord_z - ord_a + 1;
+
+    let s = '';
+
+    while (n >= 0) {
+        s = String.fromCharCode((n % len) + ord_a) + s;
+
+        n = Math.floor(n / len) - 1;
+    }
+
+    return s.toUpperCase();
+}
+
+// Write matrix data to JSON file
+fs.writeFile('matrix_data.json', JSON.stringify(matrix, null, 2), (err) => {
+    if (err) throw err;
+    console.log('Data has been written to matrix_data.json');
+});
